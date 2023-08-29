e5b9ed488748b8069f13d914a774559213dfc07e
Diego Saavedra <dsaavedra88@gmail.com>
2023-08-28 22:27:23 -0500

    Unidad 1

---
+++ Unidad1/code/actividad.py
@@ -0,0 +1,6 @@
+# Escribe un programa que solicite al usuario su nombre y luego imprima un mensaje de 
+# bienvenida personalizado.
+
+nombre = "Diego"
+
+print("Bienvenido al Curso de Python,", nombre)
